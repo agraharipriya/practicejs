4cf405bd1137ff229c68e35e068b01355e74b81c
Priya Agrahari <cooolpriya2@gmail.com>
2024-09-30 15:58:18 +0530

    datatypes and Conversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"   //  treat all Js code as newer version.
+
+//alert (" hey this cant be used")   // we are using nodejs , not browser
+
+let name="priya"
+let age=23
+let isLoggedIn= false
+let state=null
+// number => range bw 2 to power 53
+//bigint
+//string => single quote and double qoute both can be used but prefer mostly double qoute "".
+//boolean  = >true/ false
+//null  =>   initialize as null  and a standalone value
+//undefined = > declare but not given any value ;
+
+//symbol => unique 
+console.log(typeof "priya"); 
+
+console.log(typeof age);
+console.log(typeof null );
+console.log(typeof undefined);
+
---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,31 @@
+let score = "22" 
+
+console.log(typeof score );
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "22"  => 22
+// null => 0
+// undefined=>NaN
+// "33abc" => NaN
+// true =>1 / false =>0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true; o=> false
+//  "" => false
+// "priya"=> true
+
+
+let someNumber=33
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
