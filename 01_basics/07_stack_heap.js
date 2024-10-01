d2aeb74ab9d8601aaa392360f6383ec5f92dee53
Priya Agrahari <cooolpriya2@gmail.com>
2024-10-01 11:59:12 +0530

    Stackandheap

---
+++ 01_basics/07_stack_heap.js
@@ -0,0 +1,21 @@
+// Stack memory is used for Primitive datatypes and Heap is used for non premitive datatypes .
+
+let myYoutubeName="priyakesathtour.com"
+
+let anotherNamer=myYoutubeName
+anotherNamer="chaiaurcode"
+// console.log(myYoutubeName);
+
+// console.log(anotherNamer);
+
+
+let user1={
+    email:"priya@gmail.com",
+    upi:"user@ybl"
+}
+
+let user2=user1
+user2.email="satyam@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
+
