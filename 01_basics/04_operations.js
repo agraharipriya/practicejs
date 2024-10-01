1d89ed51d6a677c4ded2f466ca20fc3ce7999ce2
Priya Agrahari <cooolpriya2@gmail.com>
2024-10-01 09:14:49 +0530

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,28 @@
+let value = 3
+let negValue =- value 
+console.log(negValue);
+
+// console.log(2**3);
+// console.log(2%4);
+// console.log(2/3);
+
+
+// console.log("1" + "2");
+// console.log("1" + 2 + 1);
+// console.log(1+3 +"4");
+// console.log((2+3)*5 %2);
+
+// console.log(+true);
+// console.log(-true);
+// console.log(+"");
+
+
+
+let num1, num2,num3
+num1 = num2 =num3 = 2+2
+
+let gameCounter=100
+gameCounter++;  //post-increment print the value before increment 
+console.log(gameCounter);
+++gameCounter;  // pre-increment print the value after increment 
+console.log(gameCounter);
